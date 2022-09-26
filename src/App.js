df5c7aa167a6071a2fa0b8c82c88428f06d8621a
Tên <lexuanlong0105@gmail.com>
2022-09-26 10:28:15 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 // import InCrease from "./components/InCrease";
 // import ChooseGift from "./components/ChooseGift";
 // import GetInput from "./components/GetInput";
@@ -6,12 +5,12 @@ import { useState } from "react";
 // import CheckBox from "./components/CheckBox";
 // import ToDoList from "./components/ToDoList";
 // import Content from "./components/Content";
-import Avatar from "./components/Avatar";
+import List from "./components/List";
 function App() {
-  const [show, setShow] = useState(false);
-  const handleToggle = () => {
-    setShow(!show)
-  }
+  //   const [show, setShow] = useState(false);
+  //   const handleToggle = () => {
+  //     setShow(!show)
+  //   }
   return (
     // <InCrease />
     // <ChooseGift />
@@ -21,10 +20,11 @@ function App() {
     // <ToDoList />
     // {show && <Content />}
 
-    <div>
-      <button onClick={handleToggle}>Toggle</button>
-      {show && <Avatar />}
-    </div>
+    // <div>
+    //   <button onClick={handleToggle}>Toggle</button>
+    //   {show && <Avatar />}
+    // </div>
+    <List />
 
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+function List() {
+    return (
+        <div className="heading">
+            <p >đây là todo app</p>
+        </div>
+    );
+}
+export default List;
